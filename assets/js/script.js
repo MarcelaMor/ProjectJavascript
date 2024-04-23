21103548d748f249d5965f6789e924e7b0daf1f7
Marcela Morales <marcelamorales90@gmail.com>
2024-04-23 10:36:51 +0000

    added code to check teh answer and respond to user

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,8 @@ document.addEventListener("DOMContentLoaded", function() {
     for (let button of buttons) {
         button.addEventListener("click", function() {
             if (this.getAttribute("data-type") === "submit") {
-                alert("You clicked Submit!");
+               checkAnswer();
+
             } else {
                 let gameType = this.getAttribute("data-type");
                 runGame(gameType);
@@ -38,8 +39,21 @@ function runGame(gameType) {
     }
 
 }
+ /** Checks the answer against the first element in the returned calculatorcorrectanswer array */
 
 function checkAnswer() {
+
+    let userAnswer = parseInt(document.getElementById("answer-box").value);
+    let calculatedAnswer = calculateCorrectAnswer();
+    let isCorrect = userAnswer === calculatedAnswer[0];
+
+    if (isCorrect) {
+        alert("Hey! You got it right! :D");
+    } else {
+        alert(`Awwww.... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+    }
+
+    runGame(calculatedAnswer[1]);
 
 }
 
@@ -47,6 +61,7 @@ function checkAnswer() {
  
  * Gets the operands (the numbers) and the operator (plus, minus etc)
  * directly from the dom, and returns correct answer */
+
 
 function calculateCorrectAnswer() {
 
