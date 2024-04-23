1333f242718b6bc7e1fd7bd5e005c355bbccb9e8
Marcela Morales <marcelamorales90@gmail.com>
2024-04-23 10:52:18 +0000

    added code so it increments score and incorrect answer amount

--- assets/js/script.js
+++ assets/js/script.js
@@ -49,8 +49,10 @@ function checkAnswer() {
 
     if (isCorrect) {
         alert("Hey! You got it right! :D");
+        incrementScore();
     } else {
         alert(`Awwww.... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+        incrementWrongAnswer();
     }
 
     runGame(calculatedAnswer[1]);
@@ -77,12 +79,20 @@ function calculateCorrectAnswer() {
     }
 
 }
+/** Gets the current score from DOM and increase it by 1 */
 
 function incrementScore() {
 
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
 }
 
+/** Gets the current incorrect score from DOM and increase it by 1 */
+
 function incrementWrongAnswer() {
+
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
 
 }
 
